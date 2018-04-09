efc65e10deb91165a6d4ea90df585f22deec486c
Daryl Chan <dckj95@gmail.com>
2018-04-10 00:12:30 +0800

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+// External imports
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <h1>Charlie</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+// External imports
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Internal imports
+import App from './components/App';
+import './style.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+//import registerServiceWorker from './registerServiceWorker';
+//registerServiceWorker();
